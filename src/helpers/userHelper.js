601de35f5e2fc3e7fd8f34e27290b7c51d1f512c
Craig Wellspring <wellspring.craig@gmail.com>
2021-11-10 21:29:16 -0600

    login

--- src/helpers/userHelper.js
+++ src/helpers/userHelper.js
@@ -1,12 +1,15 @@
+import axios from 'axios';
 import firebase from 'firebase/app';
+import firebaseConfig from './firebaseHelper';
+
+const dbUrl = firebaseConfig.databaseURL;
 
 const getCurrentUsersUid = () => firebase.auth().currentUser?.uid;
 
-const getUserByUid = (uid) => new Promise((resolve) => {
-  // TODO: Get single user ingo based on uid
-  const currentUserInfo = { uid };
-  resolve(currentUserInfo);
-});
+const getUserByUid = async (uid) => {
+  const response = await axios.get(`${dbUrl}/users.json?orderBy="uid"&equalTo="${uid}"`);
+  return response;
+};
 
 export {
   getUserByUid,
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ import Create from '../views/Create';
 import Hearts from '../views/Hearts';
 import Feed from '../views/Feed';
 import User from '../views/User';
+import Login from '../views/Login';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path={['/', '/feed']} component={Feed} />
+      <Route exact path="/login" component={Login} />
       <Route exact path="/browse" component={Browse} />
       <Route exact path="/create" component={Create} />
       <Route exact path="/hearts" component={Hearts} />
